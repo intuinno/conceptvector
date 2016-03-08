3a793530093b847a7c3a1b635e9358634ef227dd
intuinno <intuinno@gmail.com>
2016-03-08 11:18:12 -0500

    Updated serverurl

---
+++ client/app/scripts/services/serverurl.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name conceptvectorApp.serverURL
+ * @description
+ * # serverURL
+ * Constant in the conceptvectorApp.
+ */
+angular.module('conceptvectorApp')
+  .constant('serverURL', 'http://conceptvector.org/api');
