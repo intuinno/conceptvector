f356841c86fbc33ca0e07000d0af27e1e82fd93e
intuinno <intuinno@gmail.com>
2016-03-17 19:19:02 -0400

    Added a clone feature

--- client/app/scripts/controllers/conceptlist.js
+++ client/app/scripts/controllers/conceptlist.js
@@ -16,18 +16,21 @@ angular.module('conceptvectorApp')
 
         $scope.concepts = [];
 
-        $http.get(serverURL + '/concepts')
-            // handle success
-            .success(function(data) {
-                console.log(data);
+        var loadConcepts = function() {
+            $http.get(serverURL + '/concepts')
+                // handle success
+                .success(function(data) {
+                    console.log(data);
 
-                $scope.concepts = data.data;
-                // $scope.$apply();
-            })
-            // handle error
-            .error(function(data) {
-                console.log(data);
-            });
+                    $scope.concepts = data.data;
+                    // $scope.$apply();
+                })
+                // handle error
+                .error(function(data) {
+                    console.log(data);
+                });
+
+        };
 
 
         $scope.isOwner = function(concept) {
@@ -61,7 +64,6 @@ angular.module('conceptvectorApp')
                 $http.delete(serverURL + '/concepts/' + concept.id)
                     // handle success
                     .success(function(data) {
-                        console.log(data);
 
                         $scope.concepts = data.data;
                         // $scope.$apply();
@@ -77,5 +79,69 @@ angular.module('conceptvectorApp')
 
         };
 
+        $scope.clone = function(concept) {
+
+            var modalInstance = $uibModal.open({
+                templateUrl: 'cloneModal.html',
+                controller: 'cloneModalCtrl',
+                size: 'sm',
+                resolve: {
+                    conceptName: function() {
+                        return concept.attributes.name;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function(newConceptName) {
+
+                var newConcept = {};
+
+                newConcept.data = {};
+
+                newConcept.data.attributes = angular.copy(concept.attributes);
+
+                newConcept.data.attributes.name = newConceptName;
+
+                newConcept.data.type = "concepts";
+
+
+                $http.post(serverURL + '/concepts', newConcept)
+                    // handle success
+                    .success(function(data) {
+                        console.log(data);
+
+                        // $scope.concepts = data.data;
+                        // $scope.$apply();
+                        loadConcepts();
+                        
+                    })
+                    // handle error
+                    .error(function(data) {
+                        console.log(data);
+                    });
+
+
+            }, function() {
+                // $log.info('Modal dismissed at: ' + new Date());
+            });
+
+        };
+
+        loadConcepts();
+
 
     }]);
+
+angular.module('conceptvectorApp')
+    .controller('cloneModalCtrl', function($scope, $uibModalInstance, conceptName) {
+
+        $scope.conceptName = 'Copy of ' + conceptName;
+
+        $scope.ok = function() {
+            $uibModalInstance.close($scope.conceptName);
+        };
+
+        $scope.cancel = function() {
+            $uibModalInstance.dismiss('cancel');
+        };
+    });
