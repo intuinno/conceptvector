72ed0c3657a4fce44836a150d2290d9caf4b0745
intuinno <intuinno@gmail.com>
2016-09-21 01:07:30 -0400

    Revert "Merge pull request #9 from intuinno/matching"
    This reverts commit cfaa73a0b32a45c71fe3c92803fa1818b276a6cb, reversing
    changes made to 3059fe7f387bbba50a219a31a41f909a1d52c1f1.

--- server/ml/matching_test.py
+++
@@ -1,9 +0,0 @@
-import matching
-
-# Examples
-old_cluster = {0:['happy', 'hello'], 1:['sad', 'depressed'], 2:['funky']}
-new_cluster = {1:['happy'], 2:['sad', 'depressed'], 0:['funky']}
-
-print 'old_cluster', old_cluster
-print 'new_cluster', new_cluster
-print matching.solve_matching(3, old_cluster, new_cluster)
